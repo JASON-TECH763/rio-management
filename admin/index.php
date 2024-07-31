1076201fa4df95d13a07a1f1e5fc3061345d42db
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-07-31 13:58:36 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -27,143 +27,101 @@
     }else{
       $error="* Please Fill all the Fileds!";
     }
-    
   }   
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    
 <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-        <title>Rio Management System</title>
-    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Rio Management System</title>
     <!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="assets/img/ a.jpg">
-
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/ a.jpg">
     <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <!-- Fontawesome CSS -->
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <!-- Main CSS -->
-        <link rel="stylesheet" href="assets/css/style.css">
-    
+    <link rel="stylesheet" href="assets/css/style.css">
     <!--[if lt IE 9]>
       <script src="assets/js/html5shiv.min.js"></script>
       <script src="assets/js/respond.min.js"></script>
     <![endif]-->
-  
     <style type="text/css">
       .divider:after,
-.divider:before {
-content: "";
-flex: 1;
-height: 1px;
-background: #eee;
-}
-.h-custom {
-height: calc(100% - 73px);
-}
-@media (max-width: 450px) {
-.h-custom {
-height: 100%;
-}
-}
+      .divider:before {
+        content: "";
+        flex: 1;
+        height: 1px;
+        background: #eee;
+      }
+      .h-custom {
+        height: calc(100% - 73px);
+      }
+      @media (max-width: 450px) {
+        .h-custom {
+          height: 100%;
+        }
+      }
+      .back-button {
+        position: absolute;
+        top: 20px;
+        left: 20px;
+      }
     </style>
-    </head>
-    <body>
-  
-    <section class="vh-100"  style="background-color: #2a2f5b; color: white; " >
+</head>
+<a href="javascript:history.back()" class="btn btn-light back-button" style="background-color: #1572e8; color: white; padding-left: 2.5rem; padding-right: 2.5rem;">Back</a>
+<body>
+<section class="vh-100" style="background-color: #2a2f5b; color: white;">
   <div class="container-fluid h-custom">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-md-9 col-lg-6 col-xl-5">
-        <img src="assets/img/1bg.jpg"
-          class="img-fluid " alt="Sample image">
+      <div class="col-md-9 col-lg-6 col-xl-5 position-relative">
+        <img src="assets/img/1bg.jpg" class="img-fluid" alt="Sample image">
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
         <form method="post">
           <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
             <div class="d-flex align-items-center mb-3 pb-1">
-                   
-            <span class="h1 fw-bold mb-0"  style="color: #FEA116;">RMS Login</span>
-            <i class="fa fa-heart fa-2x me-3"></i>
-
-                  </div>
-
-            
+              <span class="h1 fw-bold mb-0" style="color: #FEA116;">RMS Login</span>
+              <i class="fa fa-heart fa-2x me-3"></i>
+            </div>
           </div>
-<p style="color:red;"><?php echo $error; ?></p>
-        
-
-          <!-- Email input -->
+          <p style="color:red;"><?php echo $error; ?></p>
           <div class="form-outline mb-4">
-            
-             <label class="form-label" name="uname" for="user">Username</label>
-            <input type="text" name="uname" id="user" class="form-control form-control-lg"
-              placeholder="Enter username" />
+            <label class="form-label" name="uname" for="user">Username</label>
+            <input type="text" name="uname" id="user" class="form-control form-control-lg" placeholder="Enter username" />
           </div>
-           <div class="d-flex">
-           
-            
+          <div class="form-outline mb-3">
+            <label class="form-label" for="pass">Password</label>
+            <input type="password" name="pass" id="psw" class="form-control form-control-lg" placeholder="Enter password" />
+            <input class="p-2" type="checkbox" onclick="myFunction()" style="margin-left: 10px; margin-top: 13px;"> <span style="margin-left: 5px;">Show password</span>
           </div>
-          <!-- Password input -->
-          <div class="form-outline mb-3 ">
-             <label class="form-label" for="pass">Password</label>
-            <input type="password" name="pass" id="psw" class="form-control form-control-lg"
-              placeholder="Enter password" />
-                <input class="p-2" type="checkbox" onclick="myFunction()" style="margin-left: 10px; margin-top: 13px;"> <span style="margin-left: 5px;">Show password</span>
-          </div>
-         
-            
-          
-             
-        
-
           <div class="d-flex justify-content-between align-items-center">
-            <!-- Checkbox -->
-             <button type="submit" name="login" class="btn btn-warning btn-lg enter"
-              style="background-color: #1572e8; color: white; padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+            <button type="submit" name="login" class="btn btn-warning btn-lg enter" style="background-color: #1572e8; color: white; padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
             <a href="forgot.php" class="text-body">Forgot password?</a>
-
-
-           
           </div>
-
-          <div class="text-center text-lg-start mt-4 pt-2">
-           
-          </div>
-
+          <div class="text-center text-lg-start mt-4 pt-2"></div>
         </form>
       </div>
     </div>
   </div>
-  
-
-   
 </section>
-
-    <!-- jQuery -->
-        <script src="assets/js/jquery-3.2.1.min.js"></script>
-    <!-- Bootstrap Core JS -->
-        <script src="assets/js/popper.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-    
-    <!-- Custom JS -->
-    <script src="assets/js/script.js"></script>
-    <script type="text/javascript">
+<!-- jQuery -->
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap Core JS -->
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- Custom JS -->
+<script src="assets/js/script.js"></script>
+<script type="text/javascript">
   function myFunction() {
-  var x = document.getElementById("psw");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
+    var x = document.getElementById("psw");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
   }
-}
 </script>
-    </script>
-    
-    </body>
-
+</body>
 </html>
