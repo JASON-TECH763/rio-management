3b42ca044a108dfbfdac4072a0fa7c3f863b8e13
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-08-02 00:13:52 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -70,7 +70,8 @@
       }
     </style>
 </head>
-<a href="javascript:history.back()" class="btn btn-light back-button" style="background-color: #1572e8; color: white;">Back</a>
+<a href="http://localhost/RIO" class="btn btn-light back-button" 
+style="background-color: #1572e8; color: white; padding-left: 5px; padding-right: 5px;">Back to Site</a>
 <body>
 <section class="vh-100" style="background-color: #2a2f5b; color: white;">
   <div class="container-fluid h-custom">
