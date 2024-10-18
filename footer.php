963a64257c5abaec68bdfd3ed98570dc3284bed6
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-10-18 17:25:27 +0800

    commit

--- footer.php
+++ footer.php
@@ -1,14 +1,5 @@
 <?php
-// Anti-HTTP Secure Headers
-header("Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;");
-header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
-header("X-Content-Type-Options: nosniff");
-header("X-Frame-Options: DENY");
-header("X-XSS-Protection: 1; mode=block");
-header("Referrer-Policy: no-referrer-when-downgrade");
-header("Permissions-Policy: geolocation=(self), microphone=()");
-header("Expect-CT: max-age=86400, enforce");
-header("Clear-Site-Data: \"cache\", \"cookies\", \"storage\", \"executionContexts\"");
+
 ?>
 
 <div class="container-fluid bg-dark text-light footer wow fadeIn" data-wow-delay="0.1s">
