4444c23af626feb765225af195c3176d10f82d3f
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-11-11 13:26:37 +0800

    commit

--- customer/index.php
+++ customer/index.php
@@ -158,7 +158,7 @@ style="background-color: #1572e8; color: white; padding-left: 5px; padding-right
     </div>
           <div class="d-flex justify-content-between align-items-center">
             <button type="submit" name="login" class="btn btn-warning btn-lg enter" style="background-color: #1572e8; color: white; padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-            <a href="create_account.php"  class="btn btn-warning btn-lg enter" style="background-color: #1572e8; color: white; padding-left: 2.5rem; padding-right: 2.5rem;">Create Account</a>
+            <a href="create_account.php"  class="btn btn-warning btn-lg enter" style="background-color: #1572e8; color: white; padding-left: 2.5rem; padding-right: 2.5rem;">Sign Up</a>
           </div>
           
         </form>
