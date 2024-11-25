868ce6c95a42ff784a672185d93b26ecddb0bc75
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-11-25 21:40:19 +0800

    commit

--- customer/create_account.php
+++ customer/create_account.php
@@ -229,7 +229,7 @@ if (isset($_POST['create_account'])) {
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="termsModalLabel">Terms and Conditions</h5>
+                    <h5 class="modal-body" id="termsModalLabel">Terms and Conditions</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
