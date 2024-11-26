afc79cc3ba40b33ed51561bc9f428aa2ae0b488a
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-11-26 16:06:49 +0800

    commit

--- booking.php
+++ booking.php
@@ -102,9 +102,6 @@ if ($conn->query($sql) === TRUE) {
     <!-- Customized Bootstrap Stylesheet -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
     <!-- Template Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
 
