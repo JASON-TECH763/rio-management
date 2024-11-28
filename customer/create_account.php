b31977ae6ee5bc11d348eacf047c2484efcf9f51
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-11-28 23:30:50 +0800

    commit

--- customer/create_account.php
+++ customer/create_account.php
@@ -115,6 +115,12 @@ if (isset($_POST['create_account'])) {
         }
     }
 }
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
