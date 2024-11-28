9afe4da906b7558d3834252e440a66c616b0128e
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-11-28 23:23:53 +0800

    commit

--- booking.php
+++ booking.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 include('config/connect.php');
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 
 // Validate terms agreement
 if (!isset($_POST['terms_agreement'])) {
