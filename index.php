bb51851ea6b449218e447eed64dcd92ff5291fe6
JASON-TECH763 <josejasoncueva402@gmail.com>
2024-12-07 08:23:26 +0800

    commit

--- index.php
+++ index.php
@@ -398,7 +398,7 @@ if ($result->num_rows > 0) {
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
 
-    <script>
+    <!--<script>
 // Disable right-click
         document.addEventListener('contextmenu', function (e) {
             e.preventDefault();
@@ -428,7 +428,7 @@ if ($result->num_rows > 0) {
         document.onselectstart = function (e) {
             e.preventDefault();
         };
-</script>
+</script>-->
 </body>
 
 </html>
