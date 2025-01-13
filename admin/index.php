967a1209722116d58df71e6eaaa86caee185e7b5
JASON-TECH763 <josejasoncueva402@gmail.com>
2025-01-13 20:26:58 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -121,11 +121,11 @@ if (!isset($_SESSION['csrf_token'])) {
           </div>
 
           <div class="form-outline mb-4">
-            <label class="form-label" for="user">Usrname</label>
+            <label class="form-label" for="user">Username</label>
             <input type="text" name="uname" id="user" class="form-control form-control-lg" placeholder="Enter username" required autocomplete="username">
           </div>
           <div class="form-outline mb-3">
-            <label class="form-label" for="pass">Password</label>
+            <label class="form-label" for="pass">Pasword</label>
             <input  
                 type="password" 
                 name="pass" 
